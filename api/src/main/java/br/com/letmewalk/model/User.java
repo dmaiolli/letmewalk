7f5f5ff650fd36ee18fa3ab778388e346ddd23a7
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2021-11-24 18:40:59 -0300

    merging mobile api

---
+++ api/src/main/java/br/com/letmewalk/model/User.java
@@ -0,0 +1,60 @@
+package br.com.letmewalk.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_USER")
+@SequenceGenerator(name = "user", sequenceName = "SQ_USER", initialValue = 1, allocationSize = 1)
+public class User {
+
+	@Id
+	@Column(name="cd_user")
+	@GeneratedValue(generator = "user", strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	@Column(name="nm_user")
+	@NotBlank(message = "O nome é obrigatório")
+	private String nome;
+	
+	@Column(name="ds_email")
+	@NotBlank(message = "O nome é obrigatório")
+	private String email;
+	
+	@Column(name="ds_senha")
+	@NotBlank(message = "A senha é obrigatória")
+	@Size(min = 5)
+	private String senha;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Contato> contatos;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<BotaoPanico> botaoPanico;
+	
+	@Embedded
+	@Valid
+	private Endereco endereco;
+	
+}
